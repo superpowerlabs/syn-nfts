67222822b687c16e60f8fc726e4285ad756fbb60
Francesco Sullo <sullof@gmail.com>
2021-11-21 10:55:49 -0800

    Fix deploy-passes.js

--- scripts/deploy-passes.js
+++ scripts/deploy-passes.js
@@ -4,6 +4,7 @@
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 require('dotenv').config()
+const {assert} = require("chai")
 const hre = require("hardhat");
 const fs = require('fs-extra')
 const path = require('path')
@@ -36,11 +37,9 @@ async function main() {
 
   const baseTokenURI = isLocalNode
       ? "http://localhost:6660/meta/SYNPASS/"
-      : "https://blueprints.syn.city/meta/SYNPASS/"
-
-  const SynCityPasses = await ethers.getContractFactory("SynCityPasses")
-  const nft = await SynCityPasses.deploy(baseTokenURI)
-  await nft.deployed()
+      : chainId === 3
+          ? "https://blueprints.syn.city/meta/SYNPASS/"
+          : "https://nft.syn.city/meta/SYNPASS/"
   const validator = isLocalNode
       ? '0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65' // hardhat #4
       : process.env.VALIDATOR
@@ -49,7 +48,12 @@ async function main() {
       ? '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC' // hardhat #2
       : process.env.OPERATOR
 
-  console.log(validator)
+  assert.isTrue(validator.length === 42)
+  assert.isTrue(operator.length === 42)
+
+  const SynCityPasses = await ethers.getContractFactory("SynCityPasses")
+  const nft = await SynCityPasses.deploy(baseTokenURI)
+  await nft.deployed()
 
   await nft.setValidatorAndOperator(validator, operator)
 
