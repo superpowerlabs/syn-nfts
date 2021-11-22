aecc1655c88b37208edbfa7cbf4963248479e46c
Francesco Sullo <sullof@gmail.com>
2021-11-21 23:53:32 -0800

    Lint

--- contracts/SynCityPasses.sol
+++ contracts/SynCityPasses.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 // Author: Francesco Sullo <francesco@sullo.co>
+// Forked from EverDragons2(.com)'s code
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
